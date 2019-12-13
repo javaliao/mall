ff1b47c8b383d7cef4679d82cf0132a5c00a441b
lzw <lzw.liao.sh@baijiantest.com>
2019-12-13 10:22:17 +0800

    初始化搭建：网站前台基础库搭建

---
+++ portal/config-server/src/main/java/com/javaliao/configserver/ConfigServerApplication.java
@@ -0,0 +1,18 @@
+package com.javaliao.configserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * 在程序的入口Application类加上@EnableConfigServer注解开启配置服务器的功能
+ */
+@EnableConfigServer
+@SpringBootApplication
+public class ConfigServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigServerApplication.class, args);
+    }
+
+}
